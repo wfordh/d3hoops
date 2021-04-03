0b7b6ceda306604481e7d016bcbefb611faebd45
Ford Higgins <wfordh@users.noreply.github.com>
2021-04-03 10:53:44 -0700

    more chaining?

--- sdv_get_game_ids.js
+++ sdv_get_game_ids.js
@@ -1,6 +1,25 @@
 var sdv = require("sportsdataverse")
 
+function getGameID(games) {
+	var gid = Array()
+	for (const game of games.games) {
+		try {
+			gid.push(game.game.gameID)
+		} catch (error) {
+			console.log("foobar")
+		}
+	}
+	console.log(gid)
+	return gid;
+}
+
 const scores = sdv.ncaa.getScoreboard(
     sport = 'basketball-men', division='d3', year=2020, month=2, day=12
     );
-scores.then(result => console.log(result.games[0]))
+scores.then(result => console.log(getGameID(result)));
+// 	result => console.log(result.len)
+// 	).then(
+// 	result => getGameID(result)
+// 	);
+
+// console.log(result)
