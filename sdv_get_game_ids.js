97173ca2bb3d35a87f3d51447a118513891aa595
Ford Higgins <wfordh@users.noreply.github.com>
2021-05-01 11:43:17 -0700

    log of days tried

--- sdv_get_game_ids.js
+++ sdv_get_game_ids.js
@@ -286,10 +286,14 @@ function splitParse(stat, split = true) {
 const scores = sdv.ncaa.getScoreboard(
   (sport = "basketball-men"),
   (division = "d3"),
-  (year = 2020),
-  (month = 2),
-  (day = 17)
+  (year = 2021),
+  (month = 3),
+  (day = 23)
 );
+// feb 2020: 14, 15, 16, 18, 19 done. 17 bad.
+// jan 2020: 19, 20, 21, 22, 23 done. 
+// feb 2019: doesn't seem to be formatting the data correctly...
+// mar 2021: 15, 16, 17, 18, 19, 23 done. 20 and 21 bad. 
 scores
   .then((result) => getGameID(result))
   .then((boxData) => getBoxScores(boxData))
