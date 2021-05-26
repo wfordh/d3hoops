91b77f1aa89de70e2ad6e7e7daf7eab35e34cf5b
Ford Higgins <wfordh@users.noreply.github.com>
2021-05-26 12:41:34 -0700

    updating command and query

--- src/dbscripts/team_season_stats.sql
+++ src/dbscripts/team_season_stats.sql
@@ -2,6 +2,7 @@
 -- Run from psql...need to automate
 -- Inspired by https://github.com/mcbarlowe/nbadbscripts/tree/master/bash_scripts
 -- Need to figure out connecting to correct db and making sure relationship exists
+-- Command: psql -f dbscripts/team_season_stats.sql -d <dbname> -U <username>
 drop materialized view if exists team_season_stats;
 create materialized view team_season_stats as
 select
@@ -10,19 +11,20 @@ select
     date_part('month', game_date) >= 9 then date_part('year', game_date) + 1
     else date_part('year', game_date)
     end as season
-  , count(distinct game_id) as num_games
-  , sum(fgm) as fgm
-  , sum(fga) as fga
-  , round(fgm / fga, 2) as fg_pct
-  , sum(fg2m) as fg2m
-  , sum(fg2a) as fg2a
-  , round(fg2m / fg2a, 2) as fg2_pct
-  , sum(fg3m) as fg3m
-  , sum(fg3a) as fg3a
-  , round(fg3m / fg3a, 2) as fg3_pct
-  , sum(ftm) as ftm
-  , sum(fta) as fta
-  , round(ftm / fta, 2) as ft_pct
+  , count(distinct bt.game_id) as num_games
+  , sum(fgm) as season_fgm
+  , sum(fga) as season_fga
+  , round(sum(fgm) / sum(fga), 2) as fg_pct
+--  , round(season_fgm / season_fga, 2) as fg_pct
+  , sum(fg2m) as season_fg2m
+  , sum(fg2a) as season_fg2a
+  , round(season_fg2m / season_fg2a, 2) as fg2_pct
+  , sum(fg3m) as season_fg3m
+  , sum(fg3a) as season_fg3a
+  , round(season_fg3m / season_fg3a, 2) as fg3_pct
+  , sum(ftm) as season_ftm
+  , sum(fta) as season_fta
+  , round(season_ftm / season_fta, 2) as ft_pct
 from box_teams_testtest as bt
 inner join games_testtest as g on
   g.game_id = bt.game_id and (g.t0_id = bt.team_id or g.t1_id = bt.team_id)
